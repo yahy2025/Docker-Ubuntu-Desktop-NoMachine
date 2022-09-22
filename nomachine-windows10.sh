74f54215ffcd6269f5007d89a65231a2e70efd7f
yahy2025 <113997287+yahy2025@users.noreply.github.com>
2022-09-22 02:07:32 +0200

    Update nomachine-windows10.sh

--- nomachine-windows10.sh
+++ nomachine-windows10.sh
@@ -16,7 +16,7 @@ function goto
 : ngrok
 clear
 echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
-read -p "Paste Ngrok Authtoken: " CRP
+CRP=2F2AAFU0RUFjAnwsmRqmolHIj4Q_6qFuZehK1VKzGQ2wpt4hM
 ./ngrok authtoken $CRP 
 
 clear
@@ -31,7 +31,7 @@ echo "au - Australia (Sydney)"
 echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
+CRP=eu
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
